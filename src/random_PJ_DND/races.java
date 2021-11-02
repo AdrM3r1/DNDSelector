874cd7d2c59d4d973868a70187c482178ad83f56
Adrian <adrmp93@gmail.com>
2021-11-02 22:10:27 +0100

    added races classes

--- src/random_PJ_DND/races.java
+++ src/random_PJ_DND/races.java
@@ -1,5 +1,25 @@
 package random_PJ_DND;
 
-public class races {
+import java.util.Arrays;
+import java.util.List;
 
+public class races {
+    public static final String[] commonRaces = {"Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf", "Halfling", "Half-Orc", "Human", "Tiefling"};
+    public static final List<String> commonRacesList = Arrays.asList(commonRaces);
+
+    public static final String[] monstrousRace = {"Bugbear", "Centaur", "Goblin", "Grung", "Hobgoblin", "Kobold", "Lizardfolk", "Minotaur", "Orc", "Yuan-Ti Pureblood"};
+    public static final List<String> monstrousRaceList = Arrays.asList(monstrousRace);
+
+    public static final String[] exoticRaces = {"Aarakocra", "Aasimar", "Fairy", "Firbolg", "Genasi", "Gith", "Goliath", "Harengon", "Kenku", "Locathah", "Tabaxi", "Tortle", "Triton"};
+    public static final List<String> exoticRacesList = Arrays.asList(exoticRaces);
+
+    public static final String[] eberronRace = {"Changeling", "Kalashtar", "Shifter", "Warforged"};
+    public static final List<String> eberronRaceList = Arrays.asList(eberronRace);
+
+    public static final String[] arcanaRace = {"Autognome", "Dhamphir", "Giff", "Hadozee", "Hexblood", "Fairy", "Owlfolk", "Plasmoid", "Rabbitfolk", "Reborn", "Revenant", "Thri-Kreen"};
+    public static final List<String> arcanaRaceList = Arrays.asList(arcanaRace);
+
+    
 }
+
+
--- src/random_PJ_DND/racesSelected.java
+++ src/random_PJ_DND/racesSelected.java
@@ -1,4 +1,9 @@
 package random_PJ_DND;
 
 public interface racesSelected {
+
+    static void human() {
+
+    }
+
 }
